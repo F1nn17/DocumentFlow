955265b7245814e2c06c5f16893a04b8f2e1ad9e
Suxov <Suxov@SHIRAKU>
2024-03-28 22:45:52 +0400

    #Change combobox

--- DocumentFlow/resource1.h
+++ DocumentFlow/resource1.h
@@ -5,6 +5,7 @@
 #define NewDocumentWindow               101
 #define IDC_FORMATFILE                  1005
 #define IDC_INPUTNAME                   1007
+#define IDC_COMBOBOXEX2                 1013
 
 // Next default values for new objects
 // 
@@ -12,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
